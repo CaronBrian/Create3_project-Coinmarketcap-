ef1f5e791a5e03793d44da677daaccd5b3e296a4
briancaron <brian.caron@student.howest.be>
2017-12-10 11:32:54 +0100

    'finished'

--- coimarketcap opdracht/js/index.js
+++ coimarketcap opdracht/js/index.js
@@ -46,7 +46,7 @@
     $('.patch-container').patchpanel();
 
 
-
+    /*contact*/
     function submitContactForm(){
     var reg = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
     var name = $('#inputName').val();
